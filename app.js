1987836e5c5b04112737350a0309076b3be3f599
John Tomaselli <jrt@gtz.com>
2013-06-04 18:06:46 -0400

    Update app.js
    Added Firebird Database connection

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express = require('express'),
   app = express(),
   RouteDir = 'server/routes',
   files = fs.readdirSync(RouteDir),
-  conf = require('./conf');
+  conf = require('./conf'),
+  fb = require('node-firebird')  ; // jrt
 
 // code & template formatters
 ss.client.formatters.add(require('ss-jade'));
@@ -23,6 +24,58 @@ ss.client.define('main', {
   code: ['libs', 'app'],
   tmpl: '*'
 });
+
+
+// jrt
+var CFG = LoadConfig();
+console.log(CFG);
+CFG.connections = CFG.connections || [];
+console.log('---------CFG.host,CFG.port, CFG.database,CFG.user,CFG.password,CFG.pagesize,CFG.role---------');
+console.log(CFG.host, CFG.port, CFG.database, CFG.user, CFG.password, CFG.pagesize, CFG.role);
+console.log('------------------');
+
+
+function LoadConfig() {
+    var cfg = {};
+    try {
+        fs.statSync(__dirname + '/server/cfg/cfg.json');
+        var sCfg = fs.readFileSync(__dirname + '/server/cfg/cfg.json', 'utf8');
+        cfg = JSON.parse(sCfg);
+        console.log('CFG ', __dirname);
+    }
+    catch (e) {
+        console.log("Error loading config " + e.message)
+    }
+    return cfg;
+}
+
+function logerror(err) {
+    console.log(err.message);
+}
+fb.attachOrCreate(
+    {
+        host: CFG.host, database: CFG.database, user: CFG.user, password: CFG.password
+    },
+    function (err, db) {
+        if (err) {
+            console.log(err.message);
+        } else {
+            database = db;
+            test1();
+            console.log("\n\r db connected " );         //,database
+        }
+    }
+);
+
+test1 = function () {
+    // we have a good connection if results are displayed!
+    qrystr = 'select ID from "Staff" where id <10';
+    database.execute(qrystr, function (err, results, fields) {
+            console.log('database.query result "Staff"  ', results);
+        },
+        logerror);
+
+};
 
 // pack / minify if product env
 if (ss.env === 'production') {
@@ -45,7 +98,8 @@ files.forEach(function (file) {
   var filepath = path.resolve('./', RouteDir, file),
     route = require(filepath);
   console.log('adding ExpressJS routes found in %s', filepath);
-  route.init(app, ss);
+    route.init(app, ss);
+  // route.init(app, ss, database);
 });
 
 // catch-all route
@@ -58,6 +112,8 @@ server = app.listen(conf.webServer.port, function () {
   console.log('web server listening on port %d in %s mode', conf.webServer.port, ss.env);
 });
 ss.start(server);
+
+
 
 // append socketstream middleware
 app.stack = ss.http.middleware.stack.concat(app.stack);
